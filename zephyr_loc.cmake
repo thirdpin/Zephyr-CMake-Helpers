9785dfa6707e77a279bf24ae667c8845b5574535
ila <ila.embsys@gmail.com>
2021-04-28 02:26:30 +0300

    Fix comments

--- zephyr_loc.cmake
+++ zephyr_loc.cmake
@@ -1,21 +1,19 @@
-/*
-    Copyright (C) 2021 Third pin, LLC <https://thirdpin.io/>
+# Copyright (C) 2021 Third pin, LLC <https://thirdpin.io/>
 
-    This file is part of CMake helpers.
+# This file is part of CMake helpers.
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-*/
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 include(${CMAKE_CURRENT_LIST_DIR}/zephyr_config.cmake)
 
