9166d144ef01ec5f2141de4922e88e700c631512
Ilya Stolyarov <i.stolyarov@thirdpin.ru>
2021-06-16 20:15:04 +0300

    Bump required CMake version to work with cmake_path

--- InitCmakeHelpers.cmake
+++ InitCmakeHelpers.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.15)
+cmake_minimum_required(VERSION 3.20)
 
 if (DEFINED WEST_PYTHON)
   message(STATUS "Expected, is run by West tool. Skip Cmake Helpers initialization")
