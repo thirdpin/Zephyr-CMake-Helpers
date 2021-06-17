5ca08d5b8d66b3e337d9c323b371181d35a274cf
Ilya Stolyarov <i.stolyarov@thirdpin.ru>
2021-06-17 12:45:27 +0300

    Remove relative APPLICATION_SOURCE_DIR handle because it is not possible if not set manually

--- InitCmakeHelpers.cmake
+++ InitCmakeHelpers.cmake
@@ -28,63 +28,6 @@ if (NOT SKIP_CMAKE_HELPERS)
   message(STATUS "##### CMake Helpers Initialization start")
   list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-  if (APPLICATION_SOURCE_DIR)
-    cmake_path(IS_RELATIVE APPLICATION_SOURCE_DIR APPLICATION_SOURCE_DIR_is_relative)
-    if (APPLICATION_SOURCE_DIR_is_relative)
-      set(APPLICATION_SOURCE_DIR_BASE "")
-      cmake_path(SET APPLICATION_SOURCE_DIR NORMALIZE ${APPLICATION_SOURCE_DIR})
-      cmake_path(SET CMAKE_CURRENT_SOURCE_DIR NORMALIZE ${CMAKE_CURRENT_SOURCE_DIR})
-
-      message(STATUS "Detected APPLICATION_SOURCE_DIR set as relative: \"${APPLICATION_SOURCE_DIR}\". "
-      "Try to fine repository base")
-
-      set(loop_guard 1)
-      set(APPLICATION_SOURCE_DIR_tmp "${APPLICATION_SOURCE_DIR}")
-      set(CMAKE_CURRENT_SOURCE_DIR_tmp "${CMAKE_CURRENT_SOURCE_DIR}")
-
-      message(STATUS "##### Start finding repository base path")
-      
-      while(1)
-        list(APPEND CMAKE_MESSAGE_INDENT "  [${loop_guard}] ")
-
-        cmake_path(GET APPLICATION_SOURCE_DIR_tmp FILENAME APPLICATION_SOURCE_DIR_tmp_filename)
-        cmake_path(GET CMAKE_CURRENT_SOURCE_DIR_tmp FILENAME CMAKE_CURRENT_SOURCE_DIR_tmp_filename)
-
-        if (NOT "${APPLICATION_SOURCE_DIR_tmp_filename}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR_tmp_filename}")
-          message(STATUS "Looks like found repository base: \"${CMAKE_CURRENT_SOURCE_DIR_tmp}\"")
-          set(APPLICATION_SOURCE_DIR_BASE "${CMAKE_CURRENT_SOURCE_DIR_tmp}")
-          set(set(APPLICATION_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR_tmp}))
-          list(POP_BACK CMAKE_MESSAGE_INDENT)
-          break()
-        endif()
-
-        message(STATUS "From paths \"${APPLICATION_SOURCE_DIR_tmp}\" and \"${CMAKE_CURRENT_SOURCE_DIR_tmp}\" "
-        "trailing part is equal: \"${APPLICATION_SOURCE_DIR_tmp_filename}\" and \"${CMAKE_CURRENT_SOURCE_DIR_tmp_filename}\"")
-
-        cmake_path(GET APPLICATION_SOURCE_DIR_tmp PARENT_PATH APPLICATION_SOURCE_DIR_tmp)
-        cmake_path(GET CMAKE_CURRENT_SOURCE_DIR_tmp PARENT_PATH CMAKE_CURRENT_SOURCE_DIR_tmp)
-
-        MATH(EXPR loop_guard "${loop_guard}+1")
-
-        if (${loop_guard} GREATER 10)
-          message(STATUS "Looks like finding is loped. Try continue with untouched APPLICATION_SOURCE_DIR")
-          list(POP_BACK CMAKE_MESSAGE_INDENT)
-          break()
-        endif()
-
-        list(POP_BACK CMAKE_MESSAGE_INDENT)
-      endwhile()
-
-      message(STATUS "##### Finish finding repository base path")
-
-      if (APPLICATION_SOURCE_DIR_BASE)
-        cmake_path(APPEND APPLICATION_SOURCE_DIR_BASE "${APPLICATION_SOURCE_DIR}" OUTPUT_VARIABLE APPLICATION_SOURCE_DIR)
-        message(STATUS "Set APPLICATION_SOURCE_DIR explicite to relation of found base path: \"${APPLICATION_SOURCE_DIR}\"")
-      endif()
-    endif()
-  endif()
-
-
   message(STATUS "ZEPHYR_BASE environment is set as \"$ENV{ZEPHYR_BASE}\"")
 
   # Set correct ZEPHYR_BASE var if we call CMake directly without west meta-tool
