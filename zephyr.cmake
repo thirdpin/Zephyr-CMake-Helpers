f9e145d15c0b52761ec49fa4de36848b6e1c41ed
MrDimLis <mrlisdim@gmail.com>
2022-05-24 21:08:28 +0300

    Rm out of tree path extra resolving

--- zephyr.cmake
+++ zephyr.cmake
@@ -18,9 +18,6 @@
 # Extract version
 include(${CMAKE_CURRENT_LIST_DIR}/zephyr_version.cmake)
 
-# Resolve out of tree paths if any enabled
-include(${CMAKE_CURRENT_LIST_DIR}/out_of_tree_paths.cmake)
-
 # Setup Zephyr and its toolchains
 if (${ZEPHYR_VERSION} VERSION_GREATER_EQUAL "2.2.99")
     # This way is the most modern one
